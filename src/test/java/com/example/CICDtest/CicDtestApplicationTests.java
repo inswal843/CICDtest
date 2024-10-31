8a21164b3f5ff3c0feab78540ee4462179bd0f75
inswal843 <inswal843@gmail.com>
2024-10-31 12:40:16 +0900

    Feat : 기본 프로젝트 업로드

---
+++ src/test/java/com/example/CICDtest/CicDtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CICDtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicDtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
